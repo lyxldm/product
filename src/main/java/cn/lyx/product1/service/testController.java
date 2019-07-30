d9f3efe518234b16a061bc739d80dcdddcc1d572
2990218338@qq.com <2990218338@qq.com>
2019-07-30 22:00:14 +0800

    6

--- src/main/java/cn/lyx/product1/service/testController.java
+++ src/main/java/cn/lyx/product1/service/testController.java
@@ -15,6 +15,7 @@ public class testController {
     System.out.println (3 );
     System.out.println (2 );
     System.out.println (4 );
+    System.out.println (6 );
     return  userService.getUserById(id);
 }
 }
